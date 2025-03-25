b07caf98a4f81bd71395b7dc3b39cb74a02a6b35
poper12 <samyakdhengle89@gmail.com>
2025-03-25 23:03:59 +0530

    config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": "6df11147cbec7d62a323f0f498c8c03a",
+  "API_HASH": "a184b91d39fc85265e232e7c323fac45",
   # Get From my.telegram.org
-  "API_ID": "23933044",
+  "API_ID": "18196402",
   #Get For @BotFather
-  "BOT_TOKEN": "5123263018:AAFhrk204_c3NTupbt5aJ87tOOUVfCcrzA4",
+  "BOT_TOKEN": "7717441829:AAHNhP853L6wFR13-vqG5nlDECq5J1xKl3c",
   # Logs Channel ID
   "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
-  "CHANNEL": "",
+  "CHANNEL": "Manga_Campus",
 }
 
 DB_URL = "mongodb+srv://justatestsubject01:HzP5SK8ZiiLHcF3o@cluster0.wizfkbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "mdb"
-    
